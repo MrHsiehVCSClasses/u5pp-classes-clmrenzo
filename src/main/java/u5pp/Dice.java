1182259ac845b8e7d19ef1e63bc8d1e3bba8f5ad
clmrenzo <142936896+clmrenzo@users.noreply.github.com>
2023-11-28 20:33:04 +0000

    added constructors and one getter

---
+++ src/main/java/u5pp/Dice.java
@@ -0,0 +1,41 @@
+public class Dice
+{
+    private int numSides = 6;
+    private int startingSide = 1;
+    private String color = "green";
+
+
+    public Dice (){
+        numSides = 6;
+        startingSide = 1;
+        color= "green";
+    }
+    
+    public Dice(int initSides){
+        numSides = initSides;
+        startingSide = 1;
+        color = "green";
+    }
+
+    public Dice (int initSides, int initStartingSide ){
+        numSides = initSides;
+        startingSide = initStartingSide;
+        color = "green";
+
+    }
+
+    public Dice (int initSides, int initStartingSide, String initColor){
+        numSides = initSides;
+        startingSide = initStartingSide;
+        color = initColor;
+
+    }
+    public int getSides(){
+        return numSides;
+    }
+
+    public int getCurrentSide(){
+        
+    }
+}
+
