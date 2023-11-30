cd408c7c66a83c8a6cf7a18fef0670d77ce8982a
clmrenzo <142936896+clmrenzo@users.noreply.github.com>
2023-11-30 20:39:36 +0000

    Finished Dice.java

--- src/main/java/u5pp/Dice.java
+++ src/main/java/u5pp/Dice.java
@@ -1,34 +1,40 @@
+package u5pp;
+
 public class Dice
 {
     private int numSides = 6;
     private int startingSide = 1;
-    private String color = "green";
+    private String curColor = "green";
 
 
     public Dice (){
         numSides = 6;
         startingSide = 1;
-        color= "green";
+        curColor= "green";
     }
     
     public Dice(int initSides){
         numSides = initSides;
         startingSide = 1;
-        color = "green";
+        curColor = "green";
     }
 
     public Dice (int initSides, int initStartingSide ){
         numSides = initSides;
         startingSide = initStartingSide;
-        color = "green";
-
+        curColor = "green";
+        if (startingSide > numSides){
+            startingSide = 1;
+        }
     }
 
     public Dice (int initSides, int initStartingSide, String initColor){
         numSides = initSides;
         startingSide = initStartingSide;
-        color = initColor;
-
+        curColor = initColor;
+        if (startingSide > numSides){
+            startingSide = 1;
+        }
     }
     public int getSides(){
         return numSides;
@@ -39,11 +45,35 @@ public class Dice
     }
 
     public String getColor(){
-        return color;
+        return curColor;
     }
 
     public String toString(){
-        System.out.println ()"The " + getColor() + getSides() + "- sided dice is showing" + getCurrentSide()) ;
+        return "The " + getColor() + " " + getSides() + "-sided dice is showing " + getCurrentSide() ;
+    }
+    public boolean equals(Dice d){
+        if (d.getCurrentSide() == this.getCurrentSide()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public int roll(){
+        startingSide = (int) (Math.random()* numSides )+ 1;
+        return startingSide;
+        
+    }
+    public void changeSide(int num) {
+        if (num > 1 && num < numSides){
+            startingSide = num;
+        }
+    }
+    public void changeNumSides(int num){
+        numSides = num;
+    }
+    public void changeColor(String color){
+        curColor = color;
     }
 }
 
